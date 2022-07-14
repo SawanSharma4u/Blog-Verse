b3db3825f8b65170c113096f4c89abbb04a29c73
Sawan Kumar Sharma <sawansharmait@gmail.com>
2022-07-14 17:26:07 +0530

    Added Authentication

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ passport.deserializeUser(function(id, done){
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/home",
+    callbackURL: "https://serene-savannah-36788.herokuapp.com/auth/google/home",
     useProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 },
 function(accessToken, refreshToken, profile, cb) {
